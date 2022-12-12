e5c87485eaba606c3bc36fd675cda54dba3e673b
Kartoshechka <Kartohan@gmail.com>
2022-12-12 23:23:35 +0200

    add cors

--- app.js
+++ app.js
@@ -33,7 +33,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use("/api/posts", commentRouter);
 app.use("/api/users", userRouter);
